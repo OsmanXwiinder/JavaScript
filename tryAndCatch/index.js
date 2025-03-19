67f13afa5e75bf68cea48a9ba2ae65f76bede417
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-19 12:26:14 +0530

    try catch

---
+++ tryAndCatch/index.js
@@ -0,0 +1,22 @@
+
+
+// function getLength(name){
+//     return name.length
+// }
+
+// const ans = getLength()
+// console.log(ans)
+
+// console.log("hi there")
+
+ 
+
+try{
+    let a;
+    console.log(a.length)
+    console.log("hi there")   
+}catch(e){
+    console.log("inside catch statment") 
+}
+
+console.log("hi there")
