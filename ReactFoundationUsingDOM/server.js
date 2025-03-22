f9dd878782d42ce68c88c89e89f4f08025b54dc1
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-23 00:08:39 +0530

    ReactFoundation started

---
+++ ReactFoundationUsingDOM/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => console.log("server started"))
